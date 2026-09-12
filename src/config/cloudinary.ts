15ff6ffe257886235f412aa95e8a87d716ef87bd
Kobe-5623 <fiv5gamein@gmail.com>
2026-09-12 18:20:23 +0800

    Create post working

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
-  cloud_name: 'kmhrnjz',
+  cloud_name: 'kmhronjz',
   api_key: '389432283618832',
   api_secret: 'upYJy-1V31Wo8GjaR7bwVWnmvKM'
 });
